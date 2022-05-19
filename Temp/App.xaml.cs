642b4ef4a7b796a41f7970912acbe806afc19e3d
knaevKMK <71128514+knaevKMK@users.noreply.github.com>
2022-05-19 06:24:34 +0300

    Add project files.

---
+++ Temp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Temp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Temp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Temp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
